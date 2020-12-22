c71241e14f1a8eed9072def29eedc01db05813e2
Oleh <oleh.m97@gmail.com>
2020-12-22 17:56:54 +0200

    20:05

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from 'react-google-maps'
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    state = {
+        address: "",
+        city: "",
+        area: "",
+        state: "",
+        zoom: 15,
+        height: 400,
+        mapPosition: {
+           lat: 0,
+           lng: 0
+        },
+        markerPosition: {
+            lat: 0,
+            lng: 0
+        }
+
+
+    }
+
+  render() {
+      const MyMapComponent = withScriptjs(withGoogleMap(props =>
+          <GoogleMap
+              defaultZoom={8}
+              defaultCenter={{ lat: -34.397, lng: 150.644 }}
+          >
+              {props.isMarkerShown  && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+          </GoogleMap>
+      ))
+    return (
+        <div className="App">
+          <MyMapComponent
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAhEqwDYyJ0jEtIFYo6aeL3jrdlQD8oSM4&callback=myMap&libraries=geometry,drawing,places"
+              loadingElement ={<div style={{height: `100%`}} />}
+              containerElement={<div style={{height: `400px`}} />}
+              mapElement={<div style={{height: `100%`}} />}
+          />
+
+        </div>
+    );
+  }
 }
 
 export default App;
