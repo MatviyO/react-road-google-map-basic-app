a8cdd96cd7ec07676ed9ec030334e75a95958e15
Oleh <oleh.m97@gmail.com>
2020-12-24 21:29:42 +0200

    up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react'
 import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps'
 import Geocode from 'react-geocode';
-
+import { Descriptions, Badge } from 'antd';
 Geocode.setApiKey("AIzaSyAhEqwDYyJ0jEtIFYo6aeL3jrdlQD8oSM4&callback=myMap")
 
 class App extends React.Component {
@@ -112,6 +112,36 @@ class App extends React.Component {
       ))
     return (
         <div className="App">
+            <div>
+                <Descriptions title="User Info" bordered>
+                    <Descriptions.Item label="Product">Cloud Database</Descriptions.Item>
+                    <Descriptions.Item label="Billing Mode">Prepaid</Descriptions.Item>
+                    <Descriptions.Item label="Automatic Renewal">YES</Descriptions.Item>
+                    <Descriptions.Item label="Order time">2018-04-24 18:00:00</Descriptions.Item>
+                    <Descriptions.Item label="Usage Time" span={2}>
+                        2019-04-24 18:00:00
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Status" span={3}>
+                        <Badge status="processing" text="Running" />
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Negotiated Amount">$80.00</Descriptions.Item>
+                    <Descriptions.Item label="Discount">$20.00</Descriptions.Item>
+                    <Descriptions.Item label="Official Receipts">$60.00</Descriptions.Item>
+                    <Descriptions.Item label="Config Info">
+                        Data disk type: MongoDB
+                        <br />
+                        Database version: 3.4
+                        <br />
+                        Package: dds.mongo.mid
+                        <br />
+                        Storage space: 10 GB
+                        <br />
+                        Replication factor: 3
+                        <br />
+                        Region: East China 1<br />
+                    </Descriptions.Item>
+                </Descriptions>,
+            </div>
           <MyMapComponent
               googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAhEqwDYyJ0jEtIFYo6aeL3jrdlQD8oSM4&callback=myMap&libraries=geometry,drawing,places"
               loadingElement ={<div style={{height: `100%`}} />}
